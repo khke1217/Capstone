8765ad83659b9aea2fd1835dbeb826e6856d40c0
bnb1212 <bnb0409@gmail.com>
2019-10-16 14:14:09 +0900

    DB insert

--- Capstone-Book-Main/Capstone-Book-Main/UserControl1.cs
+++ Capstone-Book-Main/Capstone-Book-Main/UserControl1.cs
@@ -7,18 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace Capstone_Book_Main
 {
     public partial class UserControl1: UserControl
     {
+        string DBroute = "MyDB.sqlite";
         public UserControl1()
         {
             InitializeComponent();
+            db_manager();
+            
         }
 
         bool canmodi = false;
 
+        private void db_manager()
+        {
+            try
+            {
+                if (!System.IO.File.Exists(DBroute))
+                {
+                    SQLiteConnection.CreateFile(DBroute);
+                }
+                // 테이블 생성
+
+                SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=MyDB.sqlite;Version=3;");
+                m_dbConnection.Open();
+
+
+                string sql = "CREATE TABLE IF NOT EXISTS R_BOOK (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "title TEXT, series TEXT, number INT, " +
+                    "count INT, volume INT, alternate_series TEXT, " +
+                    "alternate_number INT, storyarc TEXT, seriesgroup TEXT, " +
+                    "alternate_count INT, year INT, month INT, DAY INT, " +
+                    "writer TEXT, penciller TEXT, inker TEXT, colorist TEXT, " +
+                    "Letterer TEXT, cover_artist TEXT, editor TEXT, publisher TEXT, " +
+                    "imprint TEXT, genre TEXT, page_count INT, language TEXT, format TEXT, " +
+                    "age_rating TEXT, blackandwhite TEXT, manga TEXT);";
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                command.ExecuteNonQuery();
+
+                m_dbConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+        }
         private void CanmodiButton_Click(object sender, EventArgs e)
         {
             if (!canmodi)
@@ -87,5 +125,7 @@ namespace Capstone_Book_Main
             canmodi = !canmodi;
         }
 
+        
     }
+
 }
